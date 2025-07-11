0971963a7e414971053d66c9e9053d379958a65c
ram-bhagat-thakur <mtjiff@gmail.com>
2025-07-11 16:23:57 +0530

    Updated main.jsx file

--- Development/event-registration-system/src/main.jsx
+++ Development/event-registration-system/src/main.jsx
@@ -6,7 +6,7 @@ import './components/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Home /> */}
-    <Events />
+    <Home />
+    {/* <Events /> */}
   </React.StrictMode>
 );
