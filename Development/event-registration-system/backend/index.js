9fc99d5972e8aef1fccd27047825c24dcd109111
ram-bhagat-thakur <mtjiff@gmail.com>
2025-07-20 21:26:37 +0530

    udated index.js to work properly

--- Development/event-registration-system/backend/index.js
+++ Development/event-registration-system/backend/index.js
@@ -4,14 +4,12 @@ const cors = require("cors")
 const  dotenv= require("dotenv")
 // const registerRoute = require('./routes/register');
 // const eventRoute = require('./routes/events.js');
-// const transporter = require('./utils/mailer'); // adjust path if needed
+const transporter = require('./utils/mailer'); // adjust path if needed
 const registrationsRoutes = require('./routes/registrations');
 const contactRoutes = require('./routes/contact');
-const adminRoutes = require('./routes/admin');
-// const router = require("./routes/register");
 
 require('dotenv').config(); // ✅ Load environment variables
-// dotenv.config();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parses form data
@@ -21,12 +19,13 @@ app.use('/api/registrations', registrationsRoutes);
 app.use('/api', require('./routes/events'));
 // ✅ Then mount your routes
 app.use('/api/contact', contactRoutes);
-// app.use('/api/admin', require('./routes/admin'));
-// app.use('/api/events', require('./routes/events'));
-// app.use('/api', require('./routes/events'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/events', require('./routes/events'));
+app.use('/api', require('./routes/events'));
 app.use('/api/register', require('./routes/register'));
 app.use('/uploads', express.static('uploads'));
-
+const adminRoutes = require('./routes/admin');
+const router = require("./routes/register");
 app.use('/api/admin', adminRoutes); // ✅ This works only if adminRoutes is a router
 
 console.log("adminRoutes type:", typeof adminRoutes);
@@ -51,18 +50,6 @@ app.get('/', (req, res) => {
 //     res.status(500).send("Email failed");
 //   }
 // });
-
-
-const path = require('path');
-// Serve frontend static files
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Fallback route: send index.html for all unmatched frontend paths
-app.get('*path', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
