82943b7b7a70dd5e3ccf12041568ce3c9bd2951f
ram-bhagat-thakur <mtjiff@gmail.com>
2025-07-20 19:55:56 +0530

    fixes port

--- Development/event-registration-system/backend/index.js
+++ Development/event-registration-system/backend/index.js
@@ -70,5 +70,5 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.log("❌ MongoDB error:", err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
