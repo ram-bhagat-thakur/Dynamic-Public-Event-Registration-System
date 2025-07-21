34f53a2bbc030f8f0598f261b29ec040d2852bbb
ram-bhagat-thakur <mtjiff@gmail.com>
2025-07-21 17:13:17 +0530

    To solve rout routing issue create a file onside public folder named _redirects

--- Development/event-registration-system/backend/index.js
+++ Development/event-registration-system/backend/index.js
@@ -35,6 +35,10 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.use((req, res, next) => {
+  console.log("📥 Incoming request:", req.method, req.url);
+  next();
+});
 
 // app.get('/test-email', async (req, res) => {
 //   try {
